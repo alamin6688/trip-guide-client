82efeb7de5a9647ed8ec47b747d5de78e6991f90
Alamin <alamin.kzs06@gmail.com>
2026-01-13 01:34:01 +0600

    Credentials include for deploy part 2

--- src/lib/server-fetch.ts
+++ src/lib/server-fetch.ts
@@ -12,20 +12,25 @@ const serverFetchHelper = async (
   const { headers, ...restOptions } = options;
   const accessToken = await getCookie("accessToken");
 
+
   //to stop recursion loop
-  if (endpoint !== "/auth/refresh-token") {
-    await getNewAccessToken();
-  }
+  // if (endpoint !== "/auth/refresh-token") {
+  //   await getNewAccessToken();
+  // }
+
+  if (accessToken && endpoint !== "/auth/login" && endpoint !== "/auth/refresh-token") {
+  await getNewAccessToken();
+}
 
   const response = await fetch(`${BACKEND_API_URL}${endpoint}`, {
     headers: {
-      Cookie: accessToken ? `accessToken=${accessToken}` : "",
+      // Cookie: accessToken ? `accessToken=${accessToken}` : "",
       ...headers,
       // ...(accessToken ? { "Authorization": `Bearer ${accessToken}` } : {}),
       // ...(accessToken ? { "Authorization": accessToken } : {}),
     },
+    credentials: "include", 
     ...restOptions,
-     credentials: "include", 
   });
 
   return response;
